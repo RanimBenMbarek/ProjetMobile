a69c47bbb93ca927d3046fe5f9d736fb6e5d6549
Chiraz_Doss <you@example.com>
2024-01-22 13:05:24 +0100

    enhanced details screen ui

--- app/src/main/java/com/example/projetmobile/DetailsActivity.kt
+++ app/src/main/java/com/example/projetmobile/DetailsActivity.kt
@@ -14,13 +14,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,33 +51,33 @@ fun DetailsActivityContent() {
     // Use the MaterialTheme function with the content parameter
     MaterialTheme {
         val yourBook = VolumeInfo(
-                allowAnonLogging = false,
-        authors = listOf("David A. Vise", "Mark Malseed"),
-        averageRating = 3.5,
-        canonicalVolumeLink = "https://books.google.com/books/about/The_Google_story.html?id=zyTCAlFPjgYC",
-        categories = listOf("Browsers (Computer programs)"), // Replace with the appropriate values
-        contentVersion = "1.1.0.0.preview.2",
-        description = "\"Here is the story behind one of the most remarkable Internet successes of our time. Based on scrupulous research and extraordinary access to Google, ...",
-        imageLinks = ImageLinks(
-            smallThumbnail = "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-            thumbnail = "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-        ),
-        industryIdentifiers = listOf(
-            IndustryIdentifier("ISBN_10", "055380457X"),
-            IndustryIdentifier("ISBN_13", "9780553804577")
-        ),
-        infoLink = "https://books.google.com/books?id=zyTCAlFPjgYC&ie=ISO-8859-1&source=gbs_api",
-        language = "en",
-        maturityRating = "MATURE", // Replace with the appropriate value
-        pageCount = 207,
-        previewLink = "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-        printType = "BOOK",
-        publishedDate = "2005-11-15",
-        publisher = "Random House Digital, Inc.",
-        ratingsCount = 136,
+            allowAnonLogging = false,
+            authors = listOf("David A. Vise", "Mark Malseed"),
+            averageRating = 3.5,
+            canonicalVolumeLink = "https://books.google.com/books/about/The_Google_story.html?id=zyTCAlFPjgYC",
+            categories = listOf("Browsers (Computer programs)"), // Replace with the appropriate values
+            contentVersion = "1.1.0.0.preview.2",
+            description = "\"Here is the story behind one of the most remarkable Internet successes of our time. Based on scrupulous research and extraordinary access to Google, ...",
+            imageLinks = ImageLinks(
+                smallThumbnail = "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                thumbnail = "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            ),
+            industryIdentifiers = listOf(
+                IndustryIdentifier("ISBN_10", "055380457X"),
+                IndustryIdentifier("ISBN_13", "9780553804577")
+            ),
+            infoLink = "https://books.google.com/books?id=zyTCAlFPjgYC&ie=ISO-8859-1&source=gbs_api",
+            language = "en",
+            maturityRating = "MATURE", // Replace with the appropriate value
+            pageCount = 207,
+            previewLink = "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            printType = "BOOK",
+            publishedDate = "2005-11-15",
+            publisher = "Random House Digital, Inc.",
+            ratingsCount = 136,
 
-        subtitle = "Subtitle", // Replace with the appropriate value
-        title = "The Google story"
+            subtitle = "Subtitle", // Replace with the appropriate value
+            title = "The Google story"
         ) // Extracted the book creation to a separate function
         BookDetailScreen(yourBook)
     }
@@ -89,123 +96,220 @@ fun BookDetailScreen(book: VolumeInfo) {
                         Icon(Icons.Default.ArrowBack, contentDescription = "Back")
                     }
                 },
-                // Set the background color
             )
         }
     ) {
         LazyColumn(
             modifier = Modifier
-                .padding(20.dp)
-                .padding(top = 50.dp)
-        ) {
+                .padding(10.dp)
+                .padding(top = 40.dp)
+      ) {
             item {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .clip(shape = RoundedCornerShape(16.dp))
-                ) {
+                Box {
                     Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
                             .height(400.dp)
-                            .clip(shape = RoundedCornerShape(16.dp))
+                            .width(450.dp)
+                            .padding(bottom = 10.dp),
+                          //  .background(color = colorResource(R.color.primary)),
+
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+
                     ) {
-                        if (book.imageLinks != null) {
-                            CoilImage(
-                                loading = {
-                                    Box(
-                                        modifier = Modifier
-                                            .fillMaxSize(),
-                                    ) {
-                                        CircularProgressIndicator(
-                                            modifier = Modifier.align(Alignment.Center),
-                                        )
-                                    }
-                                },
-                                imageModel = book.imageLinks.thumbnail,
-                                contentScale = ContentScale.Fit,
-                            )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            if (book.imageLinks != null) {
+                                CoilImage(
+                                    loading = {
+                                        Box(
+                                        ) {
+                                            CircularProgressIndicator(
+                                                modifier = Modifier.align(Alignment.Center),
+                                            )
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .height(300.dp)
+                                        .padding(10.dp)
+                                        .width(200.dp),
+                                    imageModel = book.imageLinks.thumbnail,
+                                    contentScale = ContentScale.Fit,
+                                )
+                            }
+                            Column(
+                                modifier = Modifier.padding(top = 15.dp),
+                                verticalArrangement = Arrangement.Center
+
+                            ) {
+                                InfoTable(
+                                    icon = Icons.Default.Star,
+                                    title = "Rating",
+                                    value = book.averageRating.toString()
+                                )
+                                Spacer(modifier = Modifier.padding(10.dp))
+                                InfoTable(
+                                    icon = painterResource(id = R.drawable.baseline_article_24),
+                                    title = "Pages ",
+                                    value = book.pageCount.toString()
+                                )
+                                Spacer(modifier = Modifier.padding(10.dp))
+                                InfoTable(
+                                    icon = painterResource(id = R.drawable.baseline_g_translate_24),
+                                    title = "Language",
+                                    value = book.language.uppercase()
+                                )
+
+                            }
+
                         }
 
-                        // Title and Authors
-                        Column(
+                        Text(
+                            text = book.title,
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 25.sp
+                            )
+
+                        )
+
+                        val authors =
+                            if (book.authors != null) book.authors.joinToString(
+                                ","
+                            ) else "Unknown"
+
+                        Text(
+                            text = "By $authors",
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                fontSize = 18.sp
+                            ),
                             modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp),
-                            verticalArrangement = Arrangement.Bottom,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(
-                                text = book.title,
-                                style = MaterialTheme.typography.titleLarge.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 20.sp
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = book.subtitle ?: "", // Handle null subtitle
-                                style = MaterialTheme.typography.titleSmall.copy(
-                                    fontSize = 15.sp
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = "By " + book.authors.joinToString(", "),
-                                style = MaterialTheme.typography.titleMedium.copy(
-                                    fontSize = 18.sp
-                                )
-                            )
-                        }
+                                .padding(top = 5.dp)
+                        )
+
+
                     }
                 }
             }
 
-            // Bottom Box with Description, Price, and Volume Description
             item {
                 Box(
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
+                       // .background(MaterialTheme.colorScheme.background)
                         .fillMaxWidth()
-                        .padding(16.dp)
+                        .padding(10.dp)
                 ) {
                     // Row with Description and Price
                     Column {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(bottom = 16.dp),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text(
-                                text = "Description",
-                                style = MaterialTheme.typography.titleMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
-                                )
-                            )
-                            Text(
-                                text = "Published at: ${book.publishedDate}",
-                                style = MaterialTheme.typography.titleSmall
-                            )
-                        }
-
-                        // Volume Description
-                        Text(
-                            text = book.description ?: "", // Handle null description
-                            style = MaterialTheme.typography.bodyMedium.copy(
-                                color = MaterialTheme.colorScheme.onSurface,
-                                fontSize = 18.sp
-                            )
+                        BookDescription(
+                            title = "Description",
+                            subtitle = "Published at: " + book.publishedDate,
+                            description = book.description ?: "No Description Specified"
+                        )
+                        Spacer(modifier = Modifier.padding(12.dp))
+                        BookDescription(
+                            title = "Categories",
+                            subtitle = "",
+                            description = book.categories.joinToString(", ") ?: "No Categories Specified"
                         )
                     }
+
                 }
             }
+
+            item {
+                Spacer(modifier = Modifier.height(16.dp)) // Adjust spacing as needed
+                Button(
+                    onClick = { /* Handle add to cart action */ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .padding(horizontal = 16.dp),
+                ) {
+                    Icon(Icons.Default.ShoppingCart, contentDescription = null)
+                    Spacer(modifier = Modifier.width(8.dp)) // Adjust spacing between icon and text
+                    Text("Add to Cart")
+                }
+            }
+
         }
     }
 }
+
+@Composable
+fun <T> InfoTable(icon: T, title: String, value: String) where T : Any {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .width(80.dp)
+          //  .background(color = colorResource(R.color.purple_200)),
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            when (icon) {
+                is ImageVector -> Icon(imageVector = icon, contentDescription = null)
+                is Painter -> Icon(painter = icon, contentDescription = null)
+                else -> error("Unsupported icon type")
+            }
+
+            Spacer(modifier = Modifier.padding(2.dp))
+            Text(
+                text = value,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(2.dp),
+        )
+    }
+}
+
+
+
+@Composable
+fun BookDescription(
+    title: String,
+    subtitle: String,
+    description: String
+) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp
+                )
+            )
+            Text(
+                text = subtitle,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium.copy(
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 18.sp
+            )
+        )
+    }
+}
+
+
+
 
 @Composable
 @Preview(showBackground = true)
