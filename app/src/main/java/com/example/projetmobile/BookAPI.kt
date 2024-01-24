bfb86c8ce8409ba3ac640019f2edfc199a5fadb4
Chiraz_Doss <you@example.com>
2024-01-24 22:04:18 +0100

    fix

--- app/src/main/java/com/example/projetmobile/BookAPI.kt
+++ app/src/main/java/com/example/projetmobile/BookAPI.kt
@@ -1,23 +1,26 @@
 package com.example.projetmobile
 
-import VolumeInfo
 import book
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BookAPI {
-    @GET("volumes?q=love&langRestrict=en&key=AIzaSyAMgls3MVSdN3YVefr5cWDvEsY32T_bYpQ")
+    companion object {
+        const val key = "AIzaSyAMgls3MVSdN3YVefr5cWDvEsY32T_bYpQ"
+    }
+
+    @GET("volumes?q=love&langRestrict=en&key=$key")
     fun getPopularBooks(): Call<book>
 
-    @GET("volumes?q=generation&langRestrict=en&key=AIzaSyAMgls3MVSdN3YVefr5cWDvEsY32T_bYpQ")
+    @GET("volumes?q=generation&langRestrict=en&key=$key")
     fun getBooks(): Call<book>
 
     @GET("volumes")
     fun searchBooks(
         @Query("q") search: String,
         @Query("langRestrict") langRestrict: String = "en",
-        @Query("key") apiKey: String = "AIzaSyAMgls3MVSdN3YVefr5cWDvEsY32T_bYpQ"
+        @Query("key") apiKey: String = key
     ): Call<book>
 }
+
